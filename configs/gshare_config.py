5576b19cde6e054b004b4cfa948beec8bd715df9
Ayokunle Amodu <ayokunle321@gmail.com>
2025-02-17 13:42:17 -0700

    feat: add gshare branch predictor config

---
+++ configs/gshare_config.py
@@ -0,0 +1,46 @@
+import m5
+from m5.objects import *
+
+# System setup
+system = System()
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = '1GHz'
+system.clk_domain.voltage_domain = VoltageDomain()
+
+system.mem_mode = 'timing'
+system.mem_ranges = [AddrRange('512MB')]
+
+# CPU setup
+system.cpu = MinorCPU()
+system.cpu.branchPred = GshareBP()  # Use Gshare branch predictor
+
+# Memory bus
+system.membus = SystemXBar()
+system.cpu.icache_port = system.membus.cpu_side_ports
+system.cpu.dcache_port = system.membus.cpu_side_ports
+
+system.cpu.createInterruptController()
+
+# Memory controller
+system.mem_ctrl = MemCtrl()
+system.mem_ctrl.dram = DDR3_1600_8x8()
+system.mem_ctrl.dram.range = system.mem_ranges[0]
+system.mem_ctrl.port = system.membus.mem_side_ports
+
+system.system_port = system.membus.cpu_side_ports
+
+# Placeholder workload binary
+binary = 'benchmarks/bfs_riscv'
+system.workload = SEWorkload.init_compatible(binary)
+
+process = Process()
+process.cmd = [binary]
+system.cpu.workload = process
+system.cpu.createThreads()
+
+root = Root(full_system=False, system=system)
+m5.instantiate()
+
+print(f"Running Gshare config on {binary}...")
+exit_event = m5.simulate()
+print(f'Exiting @ tick {m5.curTick()} because {exit_event.getCause()}')
